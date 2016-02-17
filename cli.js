c0ebebc9e6a6c636aa286249e991525c11ad30f6
CodeDotJS <rushforlinux@gmail.com>
2016-02-18 01:10:42 +0530

    directory sec

--- cli.js
+++ cli.js
@@ -5,8 +5,9 @@
 const fs = require('fs');
 const url = require('url');
 const http = require('follow-redirects').http;
-
+const https = require('follow-redirects').https;
 const colors = require('colors');
+const mkdirp = require('mkdirp');
 
 colors.setTheme({
     error: ['red', 'bold']
@@ -26,6 +27,11 @@ const argv = require('yargs')
     .describe('u', 'ID of facebook user')
     .describe('n', 'File name')
     .argv;
+    
+var image_in = mkdirp('/home/rishi/Downloads/Facebook_Images', function (err) {
+    if (err) console.error(err)
+    else console.log('pow!')
+});
 
 var file = fs.createWriteStream(argv.n+".jpg");
 
@@ -33,5 +39,6 @@ var start_downlaod = http.get('http://graph.facebook.com/' + argv.u + '/picture?
     res.pipe(file);
 }).on('error', function(err) {
     console.error(err);
-    console.log(image_url + ' downloaded to ' + image_in);
 });
+
+fs.mkdirSync(path, mode);
