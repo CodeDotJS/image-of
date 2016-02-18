c10447315847f02cd20241e031b28b3cc51fe5b2
CodeDotJS <rushforlinux@gmail.com>
2016-02-19 01:36:43 +0530

    testing

--- cli.js
+++ cli.js
@@ -9,17 +9,17 @@ const http = require('follow-redirects').http;
 const colors = require('chalk');
 
 const argv = require('yargs')
+
     .usage(colors.cyan('\nUsage: $0 -u [/user.id] -n [file name]'))
     .demand(['u', 'n'])
     .describe('u', 'ID of facebook user')
     .describe('n', 'File name')
     .argv;
-console.log("Please Wait")
-const file = fs.createWriteStream(argv.n+".jpg");
-
-const start_downlaod = http.get('http://graph.facebook.com/' + argv.u + '/picture?width=80', function(res) {
-    res.pipe(file);
-    console.log('Done');
-}).on('error', function(err) {
-    console.error(err);
+console.log('Please Wait');
+const file = fs.createWriteStream(argv.n);
+http.get('http://graph.facebook.com/' + argv.u + '/picture?width=80', function (res) {
+	res.pipe(file);
+	console.log('Done');
+}).on('error', function (err) {
+	console.error(err);
 });
