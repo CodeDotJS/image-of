3d9e8710842d2d804e3358973a0e3bf4978cc2a2
CodeDotJS <rushforlinux@gmail.com>
2016-02-19 00:51:23 +0530

    [extended string obj]

--- cli.js
+++ cli.js
@@ -3,34 +3,21 @@
 'use strict';
 
 const fs = require('fs');
-const url = require('url');
+
 const http = require('follow-redirects').http;
-const https = require('follow-redirects').https;
-const colors = require('colors');
 
-colors.setTheme({
-    error: ['red', 'bold']
-});
-
-colors.setTheme({
-    info: ['cyan', 'bold']
-});
-
-colors.setTheme({
-    normal: ['green', 'bold']
-});
+const colors = require('chalk');
 
 const argv = require('yargs')
-    .usage('\nUsage: $0 -u [/user.id] -n [file name]'.info)
+    .usage(colors.cyan('\nUsage: $0 -u [/user.id] -n [file name]'))
     .demand(['u', 'n'])
     .describe('u', 'ID of facebook user')
     .describe('n', 'File name')
     .argv;
-    
 console.log("Please Wait")
-var file = fs.createWriteStream(argv.n+".jpg");
+const file = fs.createWriteStream(argv.n+".jpg");
 
-var start_downlaod = http.get('http://graph.facebook.com/' + argv.u + '/picture?width=80', function(res) {
+const start_downlaod = http.get('http://graph.facebook.com/' + argv.u + '/picture?width=80', function(res) {
     res.pipe(file);
     console.log('Done');
 }).on('error', function(err) {
