ca8cf0e3e40b2254aade43823b7f8ad3a62a7be2
CodeDotJS <rushforlinux@gmail.com>
2016-02-20 11:44:18 +0530

    [ needed => ]

--- cli.js
+++ cli.js
@@ -6,20 +6,33 @@ const fs = require('fs');
 
 const http = require('follow-redirects').http;
 
-const colors = require('chalk');
+const colors = require('colors');
+
+colors.setTheme({
+	error: ['red', 'bold']
+});
+
+colors.setTheme({
+	info: ['cyan', 'bold']
+});
+
+colors.setTheme({
+	normal: ['green', 'bold']
+});
 
 const argv = require('yargs')
+  .usage(colors.cyan('\nUsage: $0 -u [/user.id] -n [file name]'))
+  .demand(['u', 'n'])
+  .describe('u', 'ID of facebook user')
+  .describe('n', 'File name')
+  .argv;
 
-    .usage(colors.cyan('\nUsage: $0 -u [/user.id] -n [file name]'))
-    .demand(['u', 'n'])
-    .describe('u', 'ID of facebook user')
-    .describe('n', 'File name')
-    .argv;
 console.log('Please Wait');
 const file = fs.createWriteStream(argv.n);
-http.get('http://graph.facebook.com/' + argv.u + '/picture?width=80', function (res) {
+http.get('http://graph.facebook.com/' + argv.u + '/picture?width=1600', function (res) {
 	res.pipe(file);
 	console.log('Done');
 }).on('error', function (err) {
 	console.error(err);
 });
+
