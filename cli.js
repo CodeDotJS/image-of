6608e65983ad5c274d138941d446350f0acf1f22
CodeDotJS <rushforlinux@gmail.com>
2016-03-24 13:59:21 +0530

    es6 | minor fixes

--- cli.js
+++ cli.js
@@ -32,7 +32,7 @@ const removeSlash = imageIn.replace('./', '');
 
 const removeDot = removeSlash.replace('/', ''); // because I don't want to.
 
-mkdirp(imageIn, function (err) {
+mkdirp(imageIn, err => {
 	if (err) {
 		console.error(err);
 	} else {
@@ -40,20 +40,22 @@ mkdirp(imageIn, function (err) {
 	}
 });
 
-setTimeout(function () {
+setTimeout(() => {
 	console.log('\n\t ❭ Downloading 		:'.directory + '	✔'.normal);
 }, 2000);
 
 const file = fs.createWriteStream(imageIn + argv.n + '.jpg');
 
-setTimeout(function () {
+setTimeout(() => {
 	console.log('\n\t ❭ In Progress 		:'.directory + '	✔'.normal);
 }, 3500);
-http.get('http://graph.facebook.com/' + argv.u + '/picture?width=1600', function (res) {
+http.get('http://graph.facebook.com/' + argv.u + '/picture?width=1600', res => {
 	res.pipe(file);
-	setTimeout(function () {
+	setTimeout(() => {
 		console.log('\n\t ❭ Image Saved in 	: '.directory + '	' + removeDot.toString().normal + ' ❭❭ ' + argv.n.toString().normal + '.jpg'.normal + '\n');
 	}, 4000);
-}).on('error', function (err) {
+}).on('error', err => {
+	process.exit(1);
 	console.error(err);
 });
+
