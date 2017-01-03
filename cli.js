323385e5e8c77dca66864912b9c7149bb69635a9
Rishi Giri <rushforlinux@gmail.com>
2017-01-03 06:56:57 +0530

    :boom: fixes, test, more

--- cli.js
+++ cli.js
@@ -3,184 +3,172 @@
 'use strict';
 
 const fs = require('fs');
-
-const http = require('follow-redirects').http;
-
+const os = require('os');
+const dns = require('dns');
 const https = require('follow-redirects').https;
-
-const mkdirp = require('mkdirp');
-
-const colors = require('colors/safe');
-
-const arrow = colors.cyan.bold('❱');
-
-const argv = require('yargs')
-
-.usage(colors.cyan.bold('\nUsage : $0 -u <command> [info] <command> [file]'))
-
-.command('u', `${arrow} facebook user's user-id`)
-
-.command('i', `${arrow} facebook user's username`)
-
-.demand(['n'])
-
-.describe('n', `${arrow} save image as`)
-
-.argv;
-
+const got = require('got');
+const fse = require('fs-extra');
+const chalk = require('chalk');
+const ora = require('ora');
+const logUpdate = require('log-update');
 const updateNotifier = require('update-notifier');
-
 const pkg = require('./package.json');
 
 updateNotifier({pkg}).notify();
 
-const argPass = argv.i;
+const spinner = ora();
+const arg = process.argv[2];
+const inf = process.argv[3];
 
-const argSlash = '/';
+const pre = chalk.cyan.bold('›');
+const pos = chalk.red.bold('›');
 
-const concatArgs = `${argSlash}${argPass}`;
+const saveMedia = `${os.homedir()}/Facebook-Images/`;
+const graph = `https://graph.facebook.com/${inf}/picture?width=1600`;
 
-const userID = {
+const fetchEye = {
 	hostname: 'www.facebook.com',
-
 	port: 443,
-
-	path: concatArgs,
-
+	path: `/${inf}`,
 	method: 'GET',
-
 	headers: {
-		'accept': 'text/html,application/json,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-
 		'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36',
-
-		'Host': 'www.facebook.com',
-
-		'Connection': 'Keep-Alive',
-
 		'Accept-Language': 'en-GB,en-US;q=0.8,en;q=0.6'
 	}
 };
 
-const folderName = './Images/';
-
-mkdirp(folderName, err => {
+fse.mkdirs(saveMedia, err => {
 	if (err) {
-		console.error(err);
-
-		process.exit(1);
-	} else {
-		console.log(colors.cyan.bold('\n ❱ Directory Created   :   ✔'));
-	}
-});
-
-function checkInternet(cb) {
-	require('dns').lookup('facebook.com', err => {
-		if (err && err.code === 'ENOTFOUND') {
-			cb(false);
-		} else {
-			cb(true);
-		}
-	});
-}
-
-// checking internet connection
-checkInternet(isConnected => {
-	if (isConnected) {
-		// do nothing : don't want to show a lot of messages.
-	} else {
-		// stop the whole process if the network is unreachable
-		console.log(colors.red.bold('\n ❱ Internet Connection   :   ✖\n'));
-
 		process.exit(1);
 	}
 });
 
-const userProfile = 'http://graph.facebook.com/';
+const dim = foll => {
+	return chalk.dim(foll);
+};
 
-const userName = argv.u;
+const showMessage = () => {
+	logUpdate();
+	spinner.text = dim('Dude, wait!');
+	spinner.start();
+	return;
+};
 
-const imageSize = '/picture?width=1600';
-
-const finalizedURL = `${userProfile}${userName}${imageSize}`;
-
-const saveArg = argv.n;
-
-const passedArgs = `${saveArg}.jpg`;
-
-if (argv.u) {
-	http.get(finalizedURL, res => {
-		if (res.statusCode === 200) {
-			const getImageIn = fs.createWriteStream(folderName + passedArgs);
-
-			res.pipe(getImageIn);
-
-			const saveFile = colors.green.bold(folderName.toString().replace('./', '').replace('/', ''));
-
-			const sendMess = colors.cyan.bold(`\n ❱ Image Saved In      :   ${saveFile} ${arrow} ${saveArg}.jpg\n`);
-
-			console.log(sendMess);
-		} else {
-			console.log(colors.red.bold('\n ❱ User Found          :   ✖ \n'));
-
+const checkConnection = () => {
+	dns.lookup('facebook.com', err => {
+		if (err) {
+			logUpdate(`\n${pos} ${dim('Please check your internet connection!')}\n`);
 			process.exit(1);
 		}
-	}).on('error', err => {
-		console.error(err);
+		logUpdate();
+		spinner.text = 'Searching...';
+		spinner.start();
+	});
+};
 
+const argList = ['-h', '--help', '-i', '--id', '-u', '--user'];
+
+if (!arg || arg === '-h' || arg === '--help' || argList.indexOf(arg) === -1) {
+	console.log(`
+  Download profile picture of any facebook user!
+
+  ${chalk.cyan('Usage')}   :  image-of [command] <username/userid>
+
+  ${chalk.cyan('Command')} :
+   -i, ${dim('--id')}         userid of facebook user
+   -u, ${dim('--user')}       username of a facebook user
+   -h, ${dim('--help')}       show help
+
+  ${chalk.cyan('Example')} :
+   $ image-of -i 4
+   $ image-of -u zuck
+  `);
+	process.exit(1);
+}
+
+const getIn = fs.createWriteStream(saveMedia + `${inf}.jpg`);
+
+const downloadMedia = opts => {
+	opts = opts || {};
+	let graph;
+	if (typeof opts !== 'string') {
+		graph = `https://graph.facebook.com/${inf}/picture?width=1600`;
+	}
+	if (typeof opts === 'string') {
+		graph = `https://graph.facebook.com/${opts}/picture?width=1600`;
+	}
+	https.get(graph, (res, cb) => {
+		res.pipe(getIn);
+		getIn.on('finish', () => {
+			logUpdate(`\n${pre} Image saved \n`);
+			spinner.stop();
+			getIn.close(cb);
+			getIn.on('error', () => {
+				process.exit(1);
+			});
+		});
+	});
+	return;
+};
+
+const removeImage = () => {
+	return fs.unlinkSync(`${saveMedia}/${inf}.jpg`);
+};
+
+if (arg === '-i' || arg === '--id') {
+	if (!inf) {
+		logUpdate(`\n${pos} ${dim('To download image, please provide userid of a facebook user!')} \n`);
+		removeImage();
 		process.exit(1);
+	}
+	showMessage();
+	checkConnection();
+	got(graph).then(() => {
+		logUpdate();
+		spinner.text = `Downloading profile picture. Hold on!`;
+		downloadMedia();
+	}).catch(err => {
+		if (err) {
+			logUpdate(`\n${pos} ${inf} ${dim('is not an userid of any facebook user!')}\n`);
+			removeImage();
+			spinner.stop();
+			process.exit(1);
+		}
 	});
 }
 
-if (argv.i) {
-	const getUserID = https.request(userID, res => {
+if (arg === '-u' || arg === '--user') {
+	if (!inf) {
+		logUpdate(`\n${pos} ${dim('To download image, please provide username of a facebook user!')} \n`);
+		removeImage();
+		process.exit(1);
+	}
+	showMessage();
+	checkConnection();
+	const getUserID = https.request(fetchEye, res => {
 		if (res.statusCode === 200) {
-			console.log(colors.cyan.bold('\n ❱ Facebook user       :   ✔'));
+			logUpdate();
+			spinner.text = `${inf} is a facebook user!`;
 		} else {
-			console.log(colors.red.bold('\n ❱ Facebook user'));
-
+			logUpdate(`\n${pos} ${inf} ${dim('is not a facebook user!')}\n`);
+			removeImage();
 			process.exit(1);
 		}
 
 		let storeData = '';
-
 		res.setEncoding('utf8');
-
 		res.on('data', d => {
 			storeData += d;
 		});
 
 		res.on('end', () => {
 			const matchPattern = new RegExp(/entity_id":"\d*/);
-
 			const arrMatches = storeData.match(matchPattern);
-
 			if (arrMatches && arrMatches[0]) {
 				const getID = arrMatches[0].replace('entity_id":"', '');
-
-				const userArgs = argv.n;
-
-				const passIt = `${userArgs}.jpg`;
-
-				const getImageIn = fs.createWriteStream(folderName + passIt);
-
-				const initialPart = 'http://graph.facebook.com/';
-
-				const finalPart = '/picture?width=1600';
-
-				const structLink = `${initialPart}${getID}${finalPart}`;
-
-				http.get(structLink, res => {
-					res.pipe(getImageIn);
-
-					const saveFile = colors.green.bold(folderName.toString().replace('./', '').replace('/', ''));
-
-					console.log(colors.cyan.bold(`\n ❱ Image Saved In      :   ${saveFile} ${arrow} ${saveArg}.jpg\n`));
-				}).on('error', err => {
-					console.error(err);
-
-					process.exit(1);
-				});
+				logUpdate();
+				spinner.text = `Downloading profile picture. Hold on!`;
+				downloadMedia(getID);
 			} else {
 				process.exit(1);
 			}
