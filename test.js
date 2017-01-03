e7b04209662714b845a3f7cc06f0644c2d6a6046
Rishi Giri <rushforlinux@gmail.com>
2017-01-03 06:57:08 +0530

    passd

--- test.js
+++ test.js
@@ -1,13 +1,13 @@
 import childProcess from 'child_process';
-
 import test from 'ava';
 
-test.cb(t => {
-	childProcess.execFile('./cli.js', ['-n anything'], {
-		cwd: __dirname
-	}, (err, stdout) => {
-		t.ifError(err);
-		t.true(stdout === '\n ❱ Directory Created   :   ✔\n');
+test.cb('default', t => {
+	const cp = childProcess.spawn('./cli.js', {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 1);
 		t.end();
 	});
 });
